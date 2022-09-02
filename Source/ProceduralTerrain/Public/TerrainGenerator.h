7fe3ef26d36752ea280aca0757d5c65ac1a697e5
Mattias Gustafsson <matte_gsson@hotmail.com>
2022-09-02 11:10:26 +0200

    Project Description

--- Source/ProceduralTerrain/Public/TerrainGenerator.h
+++ Source/ProceduralTerrain/Public/TerrainGenerator.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Procedural Terrain Generation made for school assignment. Created by Mattias Gustafsson (Walazabi): https://github.com/Walazabi/ProceduralTerrain
 
 #pragma once
 
